d5cb37bb40bf2a36db634ebc25c9a79d84536cd4
shantelle wambui <shantellewambuikungu@gmail.com>
2025-07-22 12:44:02 +0300

    Complete react-hooks-state-events-mini-project

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [tasks, setTasks] = useState(TASKS);
+
+  const filteredTasks = tasks.filter(task =>
+    selectedCategory === "All" ? true : task.category === selectedCategory
+  );
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+function handleDeleteTask(taskText) {
+    const updatedTasks = tasks.filter(task => task.text !== taskText);
+    setTasks(updatedTasks);
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+       categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+        />
+      <NewTaskForm 
+       categories={CATEGORIES}
+        onTaskFormSubmit={handleAddTask}
+      />
+      <TaskList  tasks = {filteredTasks} onDelete={handleDeleteTask}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Task() {
+function Task( {text, category, onDelete}) {
+  const handleClick = () => {
+    if (typeof onDelete === "function") {
+      onDelete(text);
+    }
+  };
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={() => onDelete(text)}>X</button>
     </div>
   );
 }
